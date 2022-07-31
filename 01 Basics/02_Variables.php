66ca7f618e8f65a5c294dab802312786317617c0
Tansen <aatansen@gmail.com>
2022-07-31 20:27:30 +0600

    Create 02_Variables.php

---
+++ 01 Basics/02_Variables.php
@@ -0,0 +1,17 @@
+<?php 
+$name = 'Bangladesh';
+$age = 123;
+$my_name = "Tansen";
+$my_name1 = "Tansen";
+$myName="Tansen";
+$MyName="Tansen";
+$word="age";
+// echo $my_name;
+// echo $$word;
+
+// Concate 
+// echo "Hello".$name;
+
+$name = "Bangladesh";
+echo "Hello $my_name";
+?>
