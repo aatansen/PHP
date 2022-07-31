803e3a21ac97f95a956bdfd46cc3b4963c2b5a82
Tansen <aatansen@gmail.com>
2022-07-31 20:51:43 +0600

    Create 02_Variables02.php

---
+++ 01 Basics/02_Variables02.php
@@ -0,0 +1,18 @@
+<?php 
+// php variable is mutable because it can be change
+$task = "Read";
+echo $task;
+echo "\n";
+$task = "Write";
+echo $task;
+echo "\n";
+
+define("PI", 3.1416);
+echo "Value of PI is ".PI;
+echo "\n";
+echo constant("PI");
+echo "\n";
+$constant = "constant";
+echo "Value of PI = {$constant('PI')}";
+
+?>
